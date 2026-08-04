f80437dce265a9bb31aebbed1af644d1a648cc35
MRGZ-prog <64190961+MRGZ-prog@users.noreply.github.com>
2026-08-04 20:48:27 +0700

    init

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:games/light_out/light_out_brick.dart';
+import 'package:games/light_out/light_out_grid.dart';
+
+void main() {
+  runApp(const MainApp());
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(body: Center(child: LightOutGrid())),
+    );
+  }
+}
